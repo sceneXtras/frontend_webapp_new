ad68d28ce310154be37d3de0fffa36488adfada5
Adrian Padurean <securiteradmin@gmail.com>
2024-03-07 17:54:54 +0200

    :sparkles: feat: Add Tailwind CSS integration

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -4,6 +4,8 @@ import { useTranslation } from 'react-i18next';
 
 import FullscreenLoading from '@/components/FullscreenLoading';
 
+import './styles/tailwind.css';
+
 const Loading = () => {
   const { t } = useTranslation('common');
 
