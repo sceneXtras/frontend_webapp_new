eadebf301c8863c6bacddfd64435be1e5c909c12
Adrian Padurean <securiteradmin@gmail.com>
2024-03-12 11:44:58 +0200

    removed stupid lint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,1 +1,9 @@
-module.exports = require('@lobehub/lint').commitlint;
+module.exports = {
+  ...require('@lobehub/lint').commitlint,
+  rules: {
+    // Override rules or disable them
+    'start-with-gitmoji': [0],
+    'subject-empty': [0],
+    'type-empty': [0],
+  },
+};
